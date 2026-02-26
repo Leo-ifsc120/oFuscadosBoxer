bac2ce77bb2e134bfd1f37692d27557fad8c19f4
leonardo <Leonardo.f.damasio@gmail.com>
2026-02-26 16:43:53 -0300

    Update

--- src/Main/PHP/telaDeLogin.php
+++ src/Main/PHP/telaDeLogin.php
@@ -15,7 +15,7 @@
 <form action="cadastro.php" method="post">
     <div class="container">
         <label for="nome">Nome: </label>
-        <input type="text" id="nome" name="nome" required autofocus><br>
+        <input type="text" id="nome" name="nome" required autofo><br>
 
         <label for="email">Email: </label>
         <input type="email" id="email" name="email" required><br>
