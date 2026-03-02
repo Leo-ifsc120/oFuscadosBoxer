410e872383ac622ef011232c4adcfced8614c3e7
leonardo <Leonardo.f.damasio@gmail.com>
2026-03-02 15:22:55 -0300

    Update

--- src/Main/PHP/telaValidacao.php
+++ src/Main/PHP/telaValidacao.php
@@ -8,32 +8,17 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 $user = $_POST["usuario"];
 $pass = $_POST["password"];
 
-$check = "select * from TblUsuario";
+$check = "select Dsusuario, Dssenha from TblUsuario where Dsususario = $user and Dssenha = $pass";
 
 $query = sqlsrv_query($conn, $check);
-
-while( $row = sqlsrv_fetch_array( $query, SQLSRV_FETCH_NUMERIC) )
+while( $row = sqlsrv_fetch_array($query) )
 {
-
-    $userCheck = $row[1];
-    $passCheck = $row[2];
-    if(strtoupper($user) !== $userCheck)
-    {
-        require "telaDeLogin.php";
-        die("<div class='error'><P class='error'>O usuario ou senha incorretos</P></div>");
-    }
-    elseif($pass !== $passCheck)
-    {
-        require "telaDeLogin.php";
-        die("<div class='error'><P class='error'>O usuario ou senha incorretos</P></div>");
-    }
-    else
-    {
-        require "cadastro.php";
-        die();
-    }
-
-
-
+    $userCheck = $row[0];
+    $passcheck = $row[2];
 }
+
+
+echo "<p> $usercheck $passCheck </p>"
+
+
 ?>
